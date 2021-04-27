a294437d1c8a8d53df48f420630c9cf1e710aa13
Eric <eric.stelt@gmail.com>
2021-04-27 12:01:04 -0500

    good god gallery added

--- deepmore/nav.js
+++ deepmore/nav.js
@@ -2,16 +2,45 @@
 // Get ToC div
     toc = document.getElementById("ToC");
 
+
     //Add a header
+    /*
     tocHeader = document.createElement("h2");
-    tocHeader.innerText="Table of contents";
+    tocHeader.innerText="Deepmore";
     toc.appendChild(tocHeader);
+    */
 
     // Create a list for the ToC entries
     tocList = document.createElement("ul");
 
+    // Get the h1 tags - ToC entries
+    headers1 = document.getElementsByTagName("h1");
+
     // Get the h2 tags - ToC entries
     headers2 = document.getElementsByTagName("h2");
+
+    // Get the h3 tags - ToC entries
+    headers3 = document.getElementsByTagName("h3");
+
+    // For each h1
+    for (e = 0; e < headers1.length; e++){
+
+      // Create an id
+      name = "h"+e;
+      headers1[e].id=name;
+
+      // a list item for the entry
+      tocHeader1 = document.createElement("p");
+      tocHeader1.className='tocHeader1';
+
+      // a link for the h1
+      tocHeader1Entry = document.createElement("a");
+      tocHeader1Entry.setAttribute("href","#"+name);
+      tocHeader1Entry.innerText=headers1[e].innerText;
+
+      tocHeader1.appendChild(tocHeader1Entry);
+      toc.appendChild(tocHeader1);
+    }
 
     // For each h2
     for (i = 0; i < headers2.length; i++){
@@ -21,38 +50,40 @@
       headers2[i].id=name;
 
       // a list item for the entry
-      tocListItem = document.createElement("li");
+      tocHeader2 = document.createElement("li");
+      tocHeader2.className='tocHeader2';
 
-      // a link for the h3
-      tocEntry = document.createElement("a");
-      tocEntry.setAttribute("href","#"+name);
-      tocEntry.innerText=headers2[i].innerText;
+      // a link for the h2
+      tocHeader2Entry = document.createElement("a");
+      tocHeader2Entry.setAttribute("href","#"+name);
+      tocHeader2Entry.innerText=headers2[i].innerText;
 
-      tocListItem.appendChild(tocEntry);
-      tocList.appendChild(tocListItem);
+      tocHeader2.appendChild(tocHeader2Entry);
+      tocList.appendChild(tocHeader2);
+
+      // For each h3
+      /*
+      for (a = 0; a < headers3.length; a++){
+
+        // Create an id
+        name = "h"+a;
+        headers3[a].id=name;
+
+        // a list item for the entry
+        tocHeader3 = document.createElement("li");
+        tocHeader3.className='tocHeader3';
+
+        // a link for the h3
+        tocHeader3Entry = document.createElement("a");
+        tocHeader3Entry.setAttribute("href","#"+name);
+        tocHeader3Entry.innerText=headers3[a].innerText;
+
+        tocHeader3.appendChild(tocHeader3Entry);
+        tocList.appendChild(tocHeader3);
+      }
+      */
     }
 
-    // Get the h3 tags - ToC entries
-    headers3 = document.getElementsByTagName("h3");
-
-    // For each h2
-    for (i = 0; i < headers3.length; i++){
-
-      // Create an id
-      name = "h"+i;
-      headers3[i].id=name;
-
-      // a list item for the entry
-      tocListItem = document.createElement("li");
-
-      // a link for the h3
-      tocEntry = document.createElement("a");
-      tocEntry.setAttribute("href","#"+name);
-      tocEntry.innerText=headers3[i].innerText;
-
-      tocListItem.appendChild(tocEntry);
-      tocList.appendChild(tocListItem);
-    }
 
 
     toc.appendChild(tocList);
